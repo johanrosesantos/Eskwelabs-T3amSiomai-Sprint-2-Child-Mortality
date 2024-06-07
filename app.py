2ee7d51ed184a03917e1311fff2d1d447d80fd29
Johan Santos <jrasantos22@gmail.com>
2024-06-08 04:19:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -68,19 +68,19 @@ def main():
                        "mothereduc_secondary",
                        "mothereduc_higher",
                        "mother_working",
-                       #"total_children_born",
-                       #"age_first_birth",
-                       #"total_births_last5years",
+                       "total_children_born",
+                       "age_first_birth",
+                       "total_births_last5years",
                        "contraceptive_folk",
                        "contraceptive_traditional",
                        "contraceptive_modern",
                        "breastfeeding_never",
                        "breastfeeding_still",
                        "child_sex_female",
-                       #"child_age_months",
-                       #"twin_1st",
-                       #"twin_2nd",
-                       #"preceding_birthinterval_months",
+                       "child_age_months",
+                       "twin_1st",
+                       "twin_2nd",
+                       "preceding_birthinterval_months",
                        "childsize_larger",
                        "childsize_average",
                        "childsize_smaller",
@@ -191,22 +191,58 @@ def main():
     st.markdown("*- Improved Drinking Water Source - piped, tube well, borehole, protected dug well/spring, rainwater, tanker truck, bottled water, water refilling station*")
     st.markdown("*- Unimproved Drinking Water Source - unprotected dug well/spring, surface water*")
 
+
+
+    
     ## Mother Information
     st.write("")
     st.subheader("Mother's Information")
 
+    ### Mother's age
+    mage_mapping = {
+        "20-24": "mother_age_20_24",
+        "25-29": "mother_age_25_29",
+        "30-34": "mother_age_30_34",
+        "35-39": "mother_age_35_39",
+        "40-44": "mother_age_40_44",
+        "45-49": "mother_age_45_49"}
+    # Display select box for selection
+    selected_mage = st.selectbox("What is the mother's age in years?", ["less than 20","20-24", "25-29", "30-34", "35-39","40-44","45-49","more than 49"], index=1)
+    # Assign values based on selection
+    for mage, element in mage_mapping.items():
+        if selected_mage == mage:
+            globals()[element] = 1
+        else:
+            globals()[element] = 0
 
-    motherage_20-24 =
-    motherage_25-29 =
-    motherage_30-34 =
-    motherage_35-39 =
-    motherage_40-44 =
-    motherage_45-49 =
+    ### Mother's Education
+    educ_mapping = {
+        "Elementary": "mothereduc_primary",
+        "Highschool": "mothereduc_secondary",
+        "College or Higher": "mothereduc_higher"}
+    # Display select box for selection
+    educ_mage = st.selectbox("What is the mother's highest educational attainment?", ["None", "Elementary", "Highschool", "College or Higher"], index=1)
+    # Assign values based on selection
+    for educ, element in educ_mapping.items():
+        if selected_educ == educ:
+            globals()[element] = 1
+        else:
+            globals()[element] = 0
+        
+    # Working
+    work_mapping = {"No": 0, "Yes": 1}
+    selected_work = st.selectbox("Are you currently employed?", ["No", "Yes"], index=0)
+    mother_working = workl_mapping[selected_work]
 
+    # Total Children Born
+    total_children_born = st.number_input("How many children have you given birth to?", min_value=1, max_value=50, value=1, step=1)
 
+    # Age at first birth
+    age_first_birth = st.number_input("At what age did you have your first child?"", min_value=1, max_value=50, value=1, step=1)
 
+    # Total births in 5 years
+    total_births_last5years = st.number_input("How many times have you given birth in the last 5 years?"", min_value=1, max_value=10, value=1, step=1)
 
-    
     ### Contraceptive
     contra_mapping = {
         "Folkloric method": "contraceptive_folk",
@@ -236,19 +272,38 @@ def main():
             globals()[element] = 1
         else:
             globals()[element] = 0
-            
+
+
+
+
+
     ## Child's Information
     st.write("")
     st.subheader("Child's Information")
 
+    # Child sex
     sex_mapping = {"Male": 0, "Female": 1}
     selected_sex = st.selectbox("Select the child's sex", ["Male", "Female"])
     child_sex_female = sex_mapping[selected_sex]
+
+    # Child age in months
+    child_age_months = st.number_input("How old is your child in months?", min_value=0, max_value=60, value=1, step=1)
+
+    # Twin Birth
+    twin_mapping = {
+        "Yes, 1st born": "twin_1st",
+        "Yes, last born": "twin_2nd"}
+    # Display select box for child size selection
+    selected_twin = st.selectbox("Is this child a twin, or part of a multiple birth?", ["No", "Yes, 1st born", "Yes, last born"], index=0)
+    # Assign values based on selected child size
+    for twin, element in twin_mapping.items():
+        if selected_twin == twin:
+            globals()[element] = 1
+        else:
+            globals()[element] = 0
     
-    #child_age_months =
-    #twin_1st =
-    #twin_2nd =
-    #preceeding_birthinterval_months =
+    # Preceding birth
+    preceeding_birthinterval_months = st.number_input("How many months apart are your current child and your previous child?", min_value=0, max_value=500, value=12, step=1)
 
     ### Child size
     childsize_mapping = {
@@ -284,29 +339,29 @@ def main():
                           toilet_unknown,
                           drinkingwater_unimproved,
                           drinkingwater_unknown,
-                          #motherage_20_24,
-                          #motherage_25_29,
-                          #motherage_30_34,
-                          #motherage_35_39,
-                          #motherage_40_44,
-                          #motherage_45_49,
-                          #mothereduc_primary,
-                          #mothereduc_secondary,
-                          #mothereduc_higher,
-                          #mother_working,
-                          #total_children_born,
-                          #age_first_birth,
-                          #total_births_last5years,
+                          mother_age_20_24,
+                          mother_age_25_29,
+                          mother_age_30_34,
+                          mother_age_35_39,
+                          mother_age_40_44,
+                          mother_age_45_49,
+                          mothereduc_primary,
+                          mothereduc_secondary,
+                          mothereduc_higher,
+                          mother_working,
+                          total_children_born,
+                          age_first_birth,
+                          total_births_last5years,
                           contraceptive_folk,
                           contraceptive_traditional,
                           contraceptive_modern,
                           breastfeeding_never,
                           breastfeeding_still,
                           child_sex_female,
-                          #child_age_months,
-                          #twin_1st,
-                          #twin_2nd,
-                          #preceeding_birthinterval_months,
+                          child_age_months,
+                          twin_1st,
+                          twin_2nd,
+                          preceeding_birthinterval_months,
                           childsize_larger,
                           childsize_average,
                           childsize_smaller,
