99d4e83142201547ff9efc48812a7f4753f53767
Johan Santos <jrasantos22@gmail.com>
2024-06-08 10:25:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,20 +5,17 @@ import pandas as pd
 import os
 import imblearn
 
-page_bg_img = '''
-<style>
-body {background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
-background-size: cover;}
-</style>
-'''
-st.markdown(page_bg_img, unsafe_allow_html=True)
-
 
 # Define the Streamlit app
 def main():
-
-
-    
+    page_bg_img = '''
+    <style>
+    body {background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+    background-size: cover;}
+    </style>
+    '''
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+ 
     st.title(":warning: Child Mortality Risk Prediction")
 
     # Determine the directory of the current script
