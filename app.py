73ce336b184bee033bdb192da31bfee77b449f2d
Johan Santos <jrasantos22@gmail.com>
2024-06-08 11:17:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def main():
     custom_css = """
     <style>
     .stApp {
-        background-image: url('https://p1.pxfuel.com/preview/331/493/252/baby-crying-infants-cry.jpg');
+        background-image: url('https://p1.pxfuel.com/preview/609/236/33/baby-feet-hands-father.jpg');
         background-size: cover;
         background-repeat: no-repeat;
         background-attachment: fixed;
